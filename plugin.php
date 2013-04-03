72995719103722bb78bc0b430d3af8e796b943e7
John Gardner <john@arconixpc.com>
2013-04-02 23:03:45 -0400

    moved view data into their respective functions
    For reasons I don't understand, when loading an external file into a
    function, it doesn't work if the function or any dependencies is needed
    by the WP front end; i.e. the Views files work on the admin side.

--- plugin.php
+++ plugin.php
@@ -40,7 +40,7 @@ class Arconix_Portfolio {
         add_image_size( 'portfolio-thumb',              275, 200 );
         add_image_size( 'portfolio-large',              620, 9999 );
 
-        add_shortcode( 'portfolio',                     array( $this, 'portfolio_shortcode' ) );
+        add_shortcode( 'portfolio',                     array( $this, 'acp_portfolio_shortcode' ) );
     }
 
     /**
@@ -98,7 +98,76 @@ class Arconix_Portfolio {
      * @return array $defaults
      */
     function portfolio_defaults() {
-        include_once( ACP_VIEWS_DIR . 'defaults.php' );
+        // Establishes plugin registration defaults for post type and taxonomy
+        $defaults = array(
+            'post_type' => array(
+                'slug' => 'portfolio',
+                'args' => array(
+                    'labels' => array(
+                        'name'                  => __( 'Portfolio',                             'acp' ),
+                        'singular_name'         => __( 'Portfolio',                             'acp' ),
+                        'add_new'               => __( 'Add New',                               'acp' ),
+                        'add_new_item'          => __( 'Add New Portfolio Item',                'acp' ),
+                        'edit'                  => __( 'Edit',                                  'acp' ),
+                        'edit_item'             => __( 'Edit Portfolio Item',                   'acp' ),
+                        'new_item'              => __( 'New Item',                              'acp' ),
+                        'view'                  => __( 'View Portfolio',                        'acp' ),
+                        'view_item'             => __( 'View Portfolio Item',                   'acp' ),
+                        'search_items'          => __( 'Search Portfolio',                      'acp' ),
+                        'not_found'             => __( 'No portfolio items found',              'acp' ),
+                        'not_found_in_trash'    => __( 'No portfolio items found in Trash',     'acp' )
+                    ),
+                    'public'            => true,
+                    'query_var'         => true,
+                    'menu_position'     => 20,
+                    'menu_icon'         => ACP_IMAGES_URL . 'portfolio-icon-16x16.png',
+                    'has_archive'       => false,
+                    'supports'          => array( 'title', 'editor', 'thumbnail' ),
+                    'rewrite'           => array( 'slug' => 'portfolio', 'with_front' => false )
+                )
+            ),
+            'taxonomy' => array(
+                'slug' => 'feature',
+                'args' => array(
+                    'labels' => array(
+                        'name'                          => __( 'Features',                              'acp' ),
+                        'singular_name'                 => __( 'Feature',                               'acp' ),
+                        'search_items'                  => __( 'Search Features',                       'acp' ),
+                        'popular_items'                 => __( 'Popular Features',                      'acp' ),
+                        'all_items'                     => __( 'All Features',                          'acp' ),
+                        'parent_item'                   => null,
+                        'parent_item_colon'             => null,
+                        'edit_item'                     => __( 'Edit Feature' ,                         'acp' ),
+                        'update_item'                   => __( 'Update Feature',                        'acp' ),
+                        'add_new_item'                  => __( 'Add New Feature',                       'acp' ),
+                        'new_item_name'                 => __( 'New Feature Name',                      'acp' ),
+                        'separate_items_with_commas'    => __( 'Separate features with commas',         'acp' ),
+                        'add_or_remove_items'           => __( 'Add or remove features',                'acp' ),
+                        'choose_from_most_used'         => __( 'Choose from the most used features',    'acp' ),
+                        'menu_name'                     => __( 'Features',                              'acp' ),
+                    ),
+                    'hierarchical'              => false,
+                    'show_ui'                   => true,
+                    'update_count_callback'     => '_update_post_term_count',
+                    'query_var'                 => true,
+                    'rewrite'                   => array( 'slug' => 'feature' )
+                )
+            ),
+            'query' => array(
+                'link'              => 'image',
+                'thumb'             => 'portfolio-thumb',
+                'full'              => 'portfolio-large',
+                'title'             => 'above',
+                'display'           => '',
+                'heading'           => 'Display',
+                'orderby'           => 'date',
+                'order'             => 'desc',
+                'posts_per_page'    => -1,
+                'terms'             => '',
+                'operator'          => 'IN',
+                'terms_orderby'      => 'name'
+            )
+        );
 
         return apply_filters( 'arconix_portfolio_defaults', $defaults );
     }
@@ -203,7 +272,8 @@ class Arconix_Portfolio {
      * @since 0.9
      * @version 1.2.0
      */
-    function portfolio_shortcode( $atts, $content = null ) {
+    function acp_portfolio_shortcode( $atts, $content = null ) {
+        wp_enqueue_script( 'arconix-portfolio-js' );
         return $this->get_portfolio_data( $atts );
     }
 
@@ -232,7 +302,168 @@ class Arconix_Portfolio {
     *
     */
     function get_portfolio_data( $args, $echo = false ) {
-        include( ACP_VIEWS_DIR . 'get-data.php' );
+        $default_args = $this->portfolio_defaults();
+        $defaults = $default_args['query'];
+
+        // Merge incoming args with the function defaults and then extract them into variables
+        $args = wp_parse_args( $args, $defaults );
+        extract( $args );
+
+        if( $title != "below" ) $title == "above"; // For backwards compatibility with "yes" and built-in data check
+
+        // Default Query arguments
+        $args = array(
+            'post_type' => 'portfolio',
+            'meta_key' => '_thumbnail_id', // Should pull only items with featured images
+            'posts_per_page' => $posts_per_page,
+            'orderby' => $orderby,
+            'order' => $order,
+        );
+
+        // If the user has defined any tax terms, then we create our tax_query and merge to our main query
+        if( $terms ) {
+            $tax_query_args = apply_filters( 'arconix_portfolio_tax_query_args', 
+                array(
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => $defaults['taxonomy']['slug'],
+                            'field' => 'slug',
+                            'terms' => $terms,
+                            'operator' => $operator  
+                        )
+                    )
+                )
+            );
+            
+            // Join the tax array to the general query
+            $args = array_merge( $args, $tax_query_args );
+        }
+
+        $return = ''; // Var that will be concatenated with our portfolio data
+
+        // Create a new query based on our own arguments
+        $portfolio_query = new WP_Query( $args );
+
+        if( $portfolio_query->have_posts() ) {
+            
+            $a = array(); // Var to hold our operate arguments
+            
+            if( $terms ) {            
+                // Translate our user-entered slug into an id we can use
+                $termid = get_term_by( 'slug', $terms, $defaults['taxonomy']['slug'] );
+                $termid = $termid->term_id;
+                
+                // Change the get_terms argument based on the shortcode $operator, but default to IN
+                switch( $operator) {
+                    case "NOT IN":
+                        $a = array( 'exclude' => $termid );
+                        break;
+                    
+                    case "IN":
+                    default:
+                        $a = array( 'include' => $termid );
+                        break;
+                }
+                
+                $a['orderby'] = $terms_orderby;
+            }
+
+            // Allow a user to filter the terms list to modify or add their own parameters.
+            $a = apply_filters( 'arconix_portfolio_get_terms', $a );
+
+            // Get the tax terms only from the items in our query
+            $get_terms = get_terms( 'feature', $a );        
+            
+            // If there are multiple terms in use, then run through our display list
+            if( count( $get_terms ) > 1 )  {
+                $display_list = '<ul class="arconix-portfolio-features">';
+                
+                if( $heading)
+                    $display_list .= "<li class='arconix-portfolio-category-title'>{$heading}</li>";
+
+                $display_list .= '<li class="active"><a href="javascript:void(0)" class="all">' . __( 'All', 'acp' ) . '</a></li>';
+
+                // Break each of the items into individual elements and modify the output
+                $term_list = '';        
+                foreach( $get_terms as $term ) {
+                    $term_list .= '<li><a href="javascript:void(0)" class="' . $term->slug . '">' . $term->name . '</a></li>';
+                }
+
+                // Return our modified list
+                $display_list .= $term_list . '</ul>';
+
+                // Allow users to filter how the 'features' are displayed
+                $return .= apply_filters( 'arconix_portfolio_display_list', $display_list );
+            }
+
+            $return .= '<ul class="arconix-portfolio-grid">';
+
+            while( $portfolio_query->have_posts() ) : $portfolio_query->the_post();
+
+                // Get the terms list
+                $get_the_terms = get_the_terms( get_the_ID(), 'feature' );
+
+                // Add each term for a given portfolio item as a data type so it can be filtered by Quicksand
+                $return .= '<li data-id="id-' . get_the_ID() . '" data-type="';
+                
+                if( $get_the_terms ) {
+                    foreach ( $get_the_terms as $term ) {
+                        $return .= $term->slug . ' ';
+                    }
+                }
+                
+                $return .= '">';
+
+                // Above image Title output
+                if( $title == "above" ) $return .= '<div class="arconix-portfolio-title">' . get_the_title() . '</div>';
+
+                // Handle the image link
+                switch( $link ) {
+                    case "page" :
+                        $return .= '<a href="' . get_permalink() . '" rel="bookmark">';                        
+                        $return .= get_the_post_thumbnail( get_the_ID(), $thumb );
+                        $return .= '</a>';
+                        break;
+
+                    case "image" :
+                        $_portfolio_img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), $full );
+                        $return .= '<a href="' . $_portfolio_img_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '" >';
+                        $return .= get_the_post_thumbnail( get_the_ID(), $thumb );
+                        $return .= '</a>';
+                        break;
+
+                    default : // If it's anything else, return nothing.
+                        break;
+                }
+
+                // Below image Title output
+                if( $title == "below" ) $return .= '<div class="arconix-portfolio-title">' . get_the_title() . '</div>';
+
+                // Display the content
+                switch( $display ) {
+                    case "content" :
+                        $return .= '<div class="arconix-portfolio-text">' . get_the_content() . '</div>';
+                        break;
+
+                    case "excerpt" :
+                        $return .= '<div class="arconix-portfolio-text">' . get_the_excerpt() . '</div>';
+                        break;
+
+                    default : // If it's anything else, return nothing.
+                        break;
+                }
+
+                $return .= '</li>';
+
+            endwhile;
+        }
+        $return .= '</ul>';
+
+    // Either echo or return the results
+    if( $echo )
+        echo $return;
+    else
+        return $return;
     }
 
     /**
@@ -244,7 +475,7 @@ class Arconix_Portfolio {
      * @version 1.2.0
      */
     function scripts() {
-        // If WP_DEBUG is true, load the non-minified versions of the files (primarily for development environments)
+        // If WP_DEBUG is true, load the non-minified versions of the files (for development environments)
         WP_DEBUG === true ? $prefix = '' : $prefix = '.min';
 
         wp_register_script( 'jquery-quicksand', ACP_JS_URL . 'jquery.quicksand' . $prefix . '.js', array( 'jquery' ), '1.3', true );
