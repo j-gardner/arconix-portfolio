46a8e4b502efd75c987df648079be3a1b8102283
John Gardner <john@arconixpc.com>
2015-09-30 17:21:26 -0400

    Refactor code to support CMB2
    The framework for #17 is in place. Needs testing and then needs to have
    strings and textdomain updated as part of #23

--- includes/class-arconix-portfolio-metaboxes.php
+++ includes/class-arconix-portfolio-metaboxes.php
@@ -12,14 +12,51 @@ class Arconix_Portfolio_Metaboxes {
      * Initialize the class
      *
      * @since   1.2.0
-     * @version 1.4.0
+     * @version 1.5.0
      * @access  public
-     * @param   string      $version    The version of this plugin.
      */
     public function __construct() {
-        add_action( 'init',             array( $this, 'metabox_init' ), 9999 );
+        add_action( 'cmb2_init',    array( $this, 'cmb2') );
+        //add_filter( 'cmb_meta_boxes',   array( $this, 'metaboxes' ) );
+    }
 
-        add_filter( 'cmb_meta_boxes',   array( $this, 'metaboxes' ) );
+    /**
+     * Define the Metabox and its fields
+     *
+     * @since   1.5.0
+     * @access  public
+     */
+    public function cmb2() {
+        // Initiate the metabox
+        $cmb = new_cmb2_box( array(
+            'id'            => 'portfolio_settings',
+            'title'         => __( 'Portfolio Setting', 'acp' ),
+            'object_types'  => array( 'portfolio' ),
+            'context'       => 'side',
+            'priority'      => 'default',
+            'show_names'    => false
+        ) );
+
+        // Add the Link Type field
+        $cmb->add_field( array(
+            'id'        => '_acp_link_type',
+            'name'      => __( 'Select Link Type', 'acp' ),
+            'desc'      => __( 'Set the hyperlink value for the portfolio item', 'acp' ),
+            'type'      => 'select',
+            'options'   => array(
+                'image'     => __( 'Image', 'acp' ),
+                'page'      => _x( 'Page', 'one side of a sheet of paper: words on a page', 'acp' ),
+                'external'  => __( 'External Link', 'acp' )
+            )
+        ));
+
+        // Add the External Link url box
+        $cmb->add_field( array(
+            'id'        => '_acp_link_value',
+            'name'      => __( 'External Link', 'acp' ),
+            'desc'      => __( 'Enter the destination hyperlink', 'acp' ),
+            'type'      => 'text'
+        ));
     }
 
     /**
